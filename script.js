8cbf6666e9774c307f24fb50fc698e8d30e70085
Natasha Fredericks <natashafredericks24@gmail.com>
2026-09-14 10:30:36 +0200

    Connect InstantDesk enquiry form delivery

--- script.js
+++ script.js
@@ -5,19 +5,3 @@ if (menu && nav) {
   menu.onclick = () => nav.classList.toggle("open");
   nav.onclick = () => nav.classList.remove("open");
 }
-
-const form = document.querySelector("#lead");
-const success = document.querySelector(".success");
-
-if (form && success) {
-  form.onsubmit = (event) => {
-    event.preventDefault();
-    form.hidden = true;
-    success.hidden = false;
-  };
-
-  success.querySelector("button").onclick = () => {
-    success.hidden = true;
-    form.hidden = false;
-  };
-}
